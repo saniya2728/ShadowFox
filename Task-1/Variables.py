ac1ddf8e273c8e44c2ee36176256807d33f8b147
saniya2728 <saniya1983124@gmail.com>
2026-07-13 22:02:13 +0530

    Create Variables.py

---
+++ Task-1/Variables.py
@@ -0,0 +1,17 @@
+#Task 1.1:create variable piand store 22/7
+pi=22/7
+print(type(pi))
+
+#Task 1.2: Explanation for 'for' keyword 
+#'for' is a reserved keyword in Python and cannot be used as a variable name.
+# We use 'for_val' instead to avoid errors.
+for_val = 4
+print(f"Value of for_val: {for_val}")
+
+#Task 1.3:Calculate Simple Interest
+# Formula: SI = (P * R * T) / 100
+P = 1000  # Principal
+R = 5     # Rate
+T = 3     # Time (3 years)
+SI = (P * R * T) / 100
+print("Simple Interest for 3 years:",SI)
